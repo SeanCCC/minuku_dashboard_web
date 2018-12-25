58df6db953fd26c8af200cebdc7c02eb6fecbff5
seanccc <sean831004@gmail.com>
2018-12-25 12:25:50 +0800

    adjust color

--- src/Components/MinukuHeatmap.js
+++ src/Components/MinukuHeatmap.js
@@ -140,8 +140,8 @@ class MinukuHeatmap extends Component {
     }
 
     const gradient = {
-      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
-      0.6: '#FAF3A5', 0.8: '#F5D98B', '1.0': '#DE9A96'
+      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#00ff00',
+      0.6: '#FAF3A5', 0.8: '#F5D98B', '1.0': '#ff0000'
     };
 
     return (
